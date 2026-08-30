96d56f8490bb41724c2d32f385f6cb6ecb095bb2
Christian Weilbach <christian@weilbach.name>
2026-08-29 20:52:40 -0700

    perf: use the JDK HTTP transport

--- test/konserve_azure_blob/test_runner.clj
+++ test/konserve_azure_blob/test_runner.clj
@@ -1,9 +1,10 @@
 (ns konserve-azure-blob.test-runner
   (:require [clojure.test :as test]
-            [konserve-azure-blob.core-test]))
+            [konserve-azure-blob.core-test]
+            [konserve-azure-blob.dependency-test]))
 
 (defn -main [& _]
-  (let [{:keys [fail error]} (test/run-tests 'konserve-azure-blob.core-test)]
+  (let [{:keys [fail error]} (test/run-tests 'konserve-azure-blob.core-test
+                                             'konserve-azure-blob.dependency-test)]
     (shutdown-agents)
     (System/exit (if (zero? (+ fail error)) 0 1))))
-
